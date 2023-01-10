1351c27b38748b6a58c438cc7366c2b7941e41bd
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2023-01-10 15:43:09 +0200

    https

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require("./app");
+const https = require("https");
 
 // const { DB_PORT } = process.env;
 
-app.listen(3001, () => {
-  console.log("Server is running");
+// app.listen(3001, () => {
+//   console.log("Server is running");
+// });
+
+https.createServer(app).listen(3001, () => {
+  console.log("server is runing");
 });
