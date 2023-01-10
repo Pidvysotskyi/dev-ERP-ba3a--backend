ad6abb3db7030172e24999980e0216e1c136b74b
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2023-01-10 17:37:15 +0200

    remove http

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const https = require("https");
 const fs = require("fs");
 const path = require("path");
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, "ssl", "ca.key")),
-  cert: fs.readFileSync(path.join(__dirname, "ssl", "ca.crt")),
-};
+// const options = {
+//   key: fs.readFileSync(path.join(__dirname, "ssl", "ca.key")),
+//   cert: fs.readFileSync(path.join(__dirname, "ssl", "ca.crt")),
+// };
 
 // const { DB_PORT } = process.env;
 
-// app.listen(3001, () => {
-//   console.log("Server is running");
-// });
-
-https.createServer(options, app).listen(3001, () => {
-  console.log("server is runing");
+app.listen(3001, () => {
+  console.log("Server is running");
 });
+
+// https.createServer(options, app).listen(3001, () => {
+//   console.log("server is runing");
+// });
