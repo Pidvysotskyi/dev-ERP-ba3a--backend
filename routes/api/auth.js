91dd983a14fe555ff999a130856aa0b05a5b2291
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2023-01-25 11:44:52 +0200

    fix

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { auth: ctrl } = require("../../controllers");
 const { validation, ctrlWrapper, auth } = require("../../middlewares");
-const { newUserSchema, loginUserSchema } = require("../../schemas/");
+const { loginUserSchema } = require("../../schemas/");
 
 const router = express.Router();
 
-router.post("/register", ctrlWrapper(ctrl.register));
+router.post("/register", auth, ctrlWrapper(ctrl.register));
 router.post("/login", validation(loginUserSchema), ctrlWrapper(ctrl.firstLogin), ctrlWrapper(ctrl.login));
 router.post("/logout", auth, ctrlWrapper(ctrl.logout));
 
