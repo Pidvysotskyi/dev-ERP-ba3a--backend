9c5a3ae3656beb185ea47704fa5800d564dbfdbd
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2023-01-31 11:27:59 +0200

    Project delete fix

--- controllers/projects/deleteProject.js
+++ controllers/projects/deleteProject.js
@@ -1,7 +1,15 @@
 const { Project } = require("../../models");
+const { NotFound } = require("http-errors");
 
 const deleteProject = async (req, res, next) => {
   const { projectKey } = req.params;
+
+  const project = await Project.getByKey(projectKey);
+
+  if (!project) {
+    throw new NotFound(`Project ${projectKey} does not exist`);
+  }
+
   await Project.delete(projectKey);
   res.status(200).json({
     message: `Project ${projectKey} deleted`,
